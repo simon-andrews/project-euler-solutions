a5e6f1ad9a7c9a68563dd2d602c6e8980705d700
Simon Andrews <simon.andrews212@gmail.com>
2017-10-22 14:07:14 -0400

    Add inefficient but "cleaner" implementation, commented out.

--- 001-multiples-of-3-and-5.hs
+++ 001-multiples-of-3-and-5.hs
@@ -1,3 +1,5 @@
 mul35 n = (sum [3,6..limit] + sum [5,10..limit]) - sum [15,30..limit] where limit = n - 1
 
+--Inefficient, but cleaner: mul35 n = sum [ x | x <- [1..limit], x `mod` 3 == 0 || x `mod` 5 == 0] where limit = n - 1
+
 main = print (mul35 1000)
